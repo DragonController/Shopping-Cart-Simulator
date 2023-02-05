1bb83a3dc3240ef8430270d78003dc4453b1c5ad
DragonController <gracefulfowler@gmail.com>
2023-02-05 00:19:31 -0800

    made some changes

--- Assets/Scripts/CartController.cs
+++ Assets/Scripts/CartController.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CartController : MonoBehaviour {
-    [SerializeField] private float moveForce;
+    [SerializeField] private float _halfMoveForce;
 
     private PlayerInput _playerInput;
-    private InputAction _moveAction;
+    private InputAction _moveAction, _lookAction, _grabAction, _retractAction, _pauseAction;
 
     private ArticulationBody _articulationBody;
     
     private void Start() {
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions["Move"];
+        _lookAction = _playerInput.actions["Look"];
+        _grabAction = _playerInput.actions["Grab"];
+        _retractAction = _playerInput.actions["Retract"];
+        _pauseAction = _playerInput.actions["Pause"];
 
         _articulationBody = GetComponent<ArticulationBody>();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         Vector2 move = _moveAction.ReadValue<Vector2>();
 
-        print(move);
+        _articulationBody.AddForceAtPosition(transform.TransformVector(0.0f, 0.0f, (move.x + move.y) * _halfMoveForce * Time.fixedDeltaTime), transform.TransformPoint(-0.5f, 0.0f, -0.5f));
+        _articulationBody.AddForceAtPosition(transform.TransformVector(0.0f, 0.0f, (-move.x + move.y) * _halfMoveForce * Time.fixedDeltaTime), transform.TransformPoint(0.5f, 0.0f, -0.5f));
+        
+        // Vector2 look = _moveAction.ReadValue<Vector2>();
 
-        _articulationBody.AddRelativeForce(Vector3.forward * move.y * moveForce);
+        // _articulationBody.AddRelativeForce(Vector3.forward * move.y * moveForce * Time.fixedDeltaTime);
     }
 }
